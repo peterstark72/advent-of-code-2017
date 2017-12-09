e9a78bd1b867ec716d268c2262551b4a91163add
Peter Stark <peterstark72@gmail.com>
2017-12-09 07:52:24 +0100

    less code, same func

--- day1/day1.go
+++ day1/day1.go
@@ -1,40 +1,36 @@
 package day1
 
 //PartOne sum if value is same as next
-func PartOne(s string) int {
+func PartOne(input string) (sum int) {
 
-	input := []rune(s)
-	fch := int(input[0]) - '0'
-	prev := fch
-	var sum, d int
-	var r rune
-	for _, r = range input[1:] {
-		d = int(r) - '0'
-		if prev == d {
-			sum += prev
+	numbers := []rune(input)
+	length := len(numbers)
+
+	for i, n := range numbers {
+
+		if n == numbers[(i+1)%length] {
+			sum += int(n - '0')
 		}
-		prev = d
+
 	}
-	if fch == prev {
-		sum += prev
-	}
-	return sum
+
+	return
 }
 
 //PartTwo sum if value is same as length/2 steps ahead
-func PartTwo(s string) int {
+func PartTwo(input string) (sum int) {
 
-	input := []rune(s)
-	length := len(input)
+	numbers := []rune(input)
+	length := len(numbers)
 	steps := length / 2
-	var sum, d1, d2, j int
-	for i, r := range input {
-		d1 = int(r) - '0'
-		j = (i + steps) % length
-		d2 = int(input[j]) - '0'
-		if d1 == d2 {
-			sum += d1
+
+	for i, n := range numbers {
+
+		if n == numbers[(i+steps)%length] {
+			sum += int(n - '0')
 		}
+
 	}
-	return sum
+
+	return
 }
